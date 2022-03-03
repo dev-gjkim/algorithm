6e351754c8456c556a04dfe72bfcc9d5c413cc08
gj.e <gijeongkim@gijeongui-MacBookPro.local>
2022-03-03 20:37:01 +0900

    트리순회

---
+++ 트리순회/inorder.js
@@ -0,0 +1,37 @@
+// 중위 순회
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree {
+    constructor(node) {
+        this.root = node;
+    }
+    inorder(node) {
+        if(node.left){
+            this.inorder(node.left)
+        }
+        console.log(node.value);
+        if(node.right){
+            this.inorder(node.right)
+        }
+    }
+
+}
+
+const tree = new Tree(new Node(1));
+tree.root.left = new Node(2);
+tree.root.left.left = new Node(4);
+tree.root.left.right = new Node(5);
+tree.root.right = new Node(3);
+tree.root.right.left = new Node(6);
+tree.root.right.right = new Node(7);
+tree.root.right.right.left = new Node(8);
+tree.root.right.right.right = new Node(9);
+
+tree.inorder(tree.root)
---
+++ 트리순회/postorder.js
@@ -0,0 +1,37 @@
+// 후위 순회
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree {
+    constructor(node) {
+        this.root = node;
+    }
+    postorder(node) {
+        if (node.left) {
+            this.postorder(node.left)
+        } if (node.right) {
+            this.postorder(node.right)
+        }
+        console.log(node.value);
+
+    }
+
+}
+
+const tree = new Tree(new Node(1));
+tree.root.left = new Node(2);
+tree.root.left.left = new Node(4);
+tree.root.left.right = new Node(5);
+tree.root.right = new Node(3);
+tree.root.right.left = new Node(6);
+tree.root.right.right = new Node(7);
+tree.root.right.right.left = new Node(8);
+tree.root.right.right.right = new Node(9);
+
+tree.postorder(tree.root)
