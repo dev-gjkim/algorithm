60e7e422f10b54fd325024fa7f5ffa1ef4a42497
gj.e <gijeongkim@gijeongui-MacBookPro.local>
2022-03-09 00:36:28 +0900

    버블정렬

---
+++ 정렬/버블정렬/sort.js
@@ -0,0 +1,15 @@
+const array = [3, 2, 6, 12, 1, 7]
+
+const sort = (array) => {
+
+    for (let i = array.length; i > 0; i--) {
+        for (let j = 0; j < i; j++) {
+            if (array[j] > array[j + 1]) {
+                [array[j], array[j + 1]] = [array[j + 1], array[j]]
+            }
+        }
+    }
+    return array;
+}
+
+console.log(sort(array))
